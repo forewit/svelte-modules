7cd24901e70f05bc25f784cb6a47886a3cd7d866
Anderson <MDAnderson@samaritan.org>
2023-06-20 14:45:36 -0400

    na

--- src/lib/Article.svelte
+++ src/lib/Article.svelte
@@ -7,57 +7,114 @@
 
   let main: HTMLElement;
 
-  // convert markdown to html
-  function markdownToHtml(markdown: string): string {
+  function markdownToHTML(markdown: string): string {
+    // extract each <style> tags from the markdown (they start with "<style>" and end with "</style>") and merge them into a single style tag string
+    const css = markdown.match(/<style>[\s\S]*?<\/style>/g)?.join("") || "";
 
-    // extract and combine each css block into a single string
-    const css = `<style>${markdown
-      .match(/```css[\s\S]*?```/g)
-      ?.map((block) => block.replace(/```css/, "").replace(/```/, ""))
-      .join("")}</style>`;
+    return (
+      css +
+      markdown
+        .replace(/\r\n<style>[\s\S]*?<\/style>/g, "")
+        .split("\r\n")
+        .map((line) => {
+          /*
+        if a line ends with {}, then it includes metadata
+        metadata looks like this: {!url #id .class @state}
+        each of the following are optional and can be in any order:
+        - url: starts with ! and is the url of the image (you can only have one url)
+        - id: starts with # and is the id of the heading/image paragraph tag (you can only have one id)
+        - class: starts with . and is the class of the heading/image/pargraph tag (you can have multiple classes)
+        - state: starts with @ and is the "state" html attribute of the heading/image/paragraph tag (you can have multiple states)
+        */
+          const metadata = line.match(/(?<=\{)[^{}]+(?=\})/g) || [];
 
-    return css + markdown
-      .replace(/```css[\s\S]*?```/g, "")
-      .split("\n")
-      .map((line) => {
-        const triggers = line.match(/\{(.+?)\}/)?.[1];
-        line = line.replace(/\{(.+?)\}/, "").trim();
+          let lineID = "",
+            lineClasses = [],
+            lineStates = [];
 
-        if (line.startsWith("#")) {
-          const level = line.match(/^(#+)/)?.[0].length || 1;
-          const title = line.replace(/^(#+\s*)/, "");
-          const slug = title
-            .toLowerCase()
-            .replace(/[^a-z0-9]+/g, "-")
-            .replace(/(^-|-$)+/g, "");
-          return `<h${level} id="${slug}" ${
-            triggers ? `triggers="${triggers}"` : ""
-          }>${title}</h${level}>`;
-        } else {
-          const imageMatch = line.match(/^!\[(.+?)\]\((.+?)\)$/);
-          const linkMatch = line.match(/^\[(.+?)\]\((.+?)\)$/);
-          return imageMatch
-            ? `<img src="${base}${imageMatch[2]}" alt="${imageMatch[1]}" ${
-                triggers ? `triggers="${triggers}"` : ""
-              }/>`
-            : linkMatch
-            ? `<a href="${base}${linkMatch[2]}" ${
-                triggers ? `triggers="${triggers}"` : ""
-              }>${linkMatch[1]}</a>`
-            : `<p ${triggers ? `triggers="${triggers}"` : ""}>${line}</p>`;
-        }
-      })
-      .join("")
-      .replace(/!\[(.+?)\]\((.+?)\)/g, `<img src="${base}$2" alt="$1" />`)
-      .replace(/\[(.+?)\]\((.+?)\)/g, `<a href="$2">$1</a>`);
+          for (let i = 0; i < metadata.length; i++) {
+            const data = metadata[i];
+            const url = data.match(/(?<=!)[^ ]+/)?.[0];
+            const id = data.match(/(?<=#)[^ ]+/)?.[0];
+            const classes = data.match(/(?<=^\.| \.)[^ ]+/g) || [];
+            const states = data.match(/(?<=@)[^ ]+/g) || [];
+
+            // if there is a url, replace that metadata tag {} with an image tag
+            if (url) {
+              line = line.replace(
+                `{${data}}`,
+                `<img src="${base}${url}" alt="${url}"${
+                  id ? ` id="${id}"` : ""
+                }${classes.length ? ` class="${classes.join(" ")}"` : ""}${
+                  states.length ? ` state="${states.join(" ")}"` : ""
+                }/>`
+              );
+            } else {
+              lineID = id;
+              lineClasses = classes;
+              lineStates = states;
+
+              // remove the metadata tag {} from the line
+              line = line.replace(`{${data}}`, "").trim();
+            }
+          }
+
+          // if line starts with "#", it's a heading
+          if (line.startsWith("#")) {
+            const level = line.match(/^(#+)/)?.[0].length || 1;
+            const text = line.replace(/^(#+\s*)/, "").trim();
+
+            return `<h${level}${lineID ? ` id="${lineID}"` : ""}${
+              lineClasses.length ? ` class="${lineClasses.join(" ")}"` : ""
+            }${
+              lineStates.length ? ` state="${lineStates.join(" ")}"` : ""
+            }>${text}</h${level}>`;
+          }
+
+          // if line starts with "{", its a opening div tag including metadata
+          else if (line.startsWith("{")) {
+            console.log(line);
+            const text = line.replace(/^{/, "").replace(/}$/, "").trim();
+
+            return `<div${lineID ? ` id="${lineID}"` : ""}${
+              lineClasses.length ? ` class="${lineClasses.join(" ")}"` : ""
+            }${
+              lineStates.length ? ` state="${lineStates.join(" ")}"` : ""
+            }>${text}${line.endsWith("}") ? "</div>" : ""}`;
+          }
+
+          // if line starts with "}", its a closing div tag
+          else if (line.startsWith("}")) {
+            return "</div>";
+          }
+
+          // skip if the line is made up entirely of one image tag
+          else if (line.match(/^<img[^>]+>$/)) {
+            return line;
+          }
+
+          // if line is empty and there is no metadata, its a line break
+          else if (line === "" && metadata.length === 0) {
+            return "<br>";
+          }
+          // otherwise, its a paragraph
+          else {
+            return `<p${lineID ? ` id="${lineID}"` : ""}${
+              lineClasses.length ? ` class="${lineClasses.join(" ")}"` : ""
+            }${
+              lineStates.length ? ` state="${lineStates.join(" ")}"` : ""
+            }>${line}</p>`;
+          }
+        })
+        .join("")
+    );
   }
 
-  // toggle classes on the main element when an element with the triggers attribute is intersected
+  // toggle classes on the main element when an element with the state attribute is intersected
   function onIntersection(entries: IntersectionObserverEntry[]): void {
     if (!main) return;
     entries.forEach((entry) => {
-      const mainClasses =
-        entry.target.getAttribute("triggers")?.split(" ") || [];
+      const mainClasses = entry.target.getAttribute("state")?.split(" ") || [];
 
       mainClasses.forEach((className) => {
         main.classList.toggle(className, entry.isIntersecting);
@@ -75,21 +132,21 @@
     // get the markdown file
     getArticle(markdownURL).then((text) => {
       // convert markdown to html
-      const markdown = markdownToHtml(text);
+      const html = markdownToHTML(text);
 
       // set the markdown as the innerHTML of the article
-      main.innerHTML = markdown;
+      main.innerHTML = html;
 
-      // create an observer to watch for elements with the triggers attribute
+      // create an observer to watch for elements with the trigger attribute
       const observer = new IntersectionObserver(onIntersection, {
         root: main,
         rootMargin: "0px",
         threshold: 0.5,
       });
 
-      // observe all elements with the triggers attribute
+      // observe all elements with the trigger attribute
       main
-        .querySelectorAll("[triggers]")
+        .querySelectorAll("[state]")
         .forEach((element) => observer.observe(element));
     });
   });
--- src/routes/(main)/blog/+page.svelte
+++ src/routes/(main)/blog/+page.svelte
@@ -4,6 +4,7 @@
 
 <h1>Welcome to the Blog!</h1>
 
+<!-- TODO: generate this list from the file system -->
 <a href="{base}/blog/test">test</a>
 <a href="{base}/blog/the-seasons">The Seasons</a>
 
